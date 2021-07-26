983591659bd221cb8b7b3f1d2bef2c661968449b
Matt Pitts <mattapitts@gmail.com>
2021-07-26 16:25:08 -0700

    feat (api):  Update request payload to include endpoint.
    Endpoint is set and retrieved from global vars service

--- packages/cloutscript-core/src/interfaces/i-get-single-profile-request-payload.ts
+++ packages/cloutscript-core/src/interfaces/i-get-single-profile-request-payload.ts
@@ -3,8 +3,10 @@
  * api/v0/get-single-profile
  * @export
  * @interface IGetSingleProfileRequestPayload
+ * @link https://github.com/bitclout/frontend/blob/9eb6a3ac4120ae95c16129222816cef906c7947e/src/app/backend-api.service.ts#L771
  */
 export interface IGetSingleProfileRequestPayload {
+    endpoint: string;
     PublicKeyBase58Check?: string;
     Username: string; 
 }
