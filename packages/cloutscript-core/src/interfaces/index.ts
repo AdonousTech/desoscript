de74c1d051ec1b5743870d639834ff568fdeee82
Matt Pitts <mattapitts@gmail.com>
2021-07-26 17:07:03 -0700

    chore (stateless users): export interface

--- packages/cloutscript-core/src/interfaces/index.ts
+++ packages/cloutscript-core/src/interfaces/index.ts
@@ -5,3 +5,5 @@ export * from './i-get-single-profile-request-payload';
 export * from './i-get-single-profile-response-payload';
 export * from './i-like-request-payload';
 export * from './i-like-response-payload';
+export * from './i-get-users-stateless-request-payload';
+export * from './i-get-users-stateless-response-payload';
