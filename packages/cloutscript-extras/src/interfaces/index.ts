d57661fe54c47da38ff8e3131fe4a2234a03befa
Matt Pitts <mattapitts@gmail.com>
2021-08-08 11:33:33 -0700

    feat (misc): Clean up file structure

--- packages/cloutscript-extras/src/interfaces/index.ts
+++ packages/cloutscript-extras/src/interfaces/index.ts
@@ -1,3 +1,10 @@
+// clout
+
+// passthrough
+export * from './passthrough/canonical-dli-instructions.enum';
+export * from './passthrough/canonical-oli-instructions.enum';
+export * from './passthrough/canonical-passthrough-errors.enum';
+// project
 export * from './project/i-project-activity';
 export * from './project/i-project-roadmap-entry';
 export * from './project/i-project-roadmap';
