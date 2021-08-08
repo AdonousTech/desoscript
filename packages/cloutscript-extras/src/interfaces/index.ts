b9a89d30951948158f2cea3075d64f967658d98e
Matt Pitts <mattapitts@gmail.com>
2021-08-08 12:27:46 -0700

    feat (passthrough) Add canonical passthrough routes

--- packages/cloutscript-extras/src/interfaces/index.ts
+++ packages/cloutscript-extras/src/interfaces/index.ts
@@ -4,6 +4,7 @@
 export * from './passthrough/canonical-dli-instructions.enum';
 export * from './passthrough/canonical-oli-instructions.enum';
 export * from './passthrough/canonical-passthrough-errors.enum';
+export * from './passthrough/canonical-passthrough-routes.enum';
 // project
 export * from './project/i-project-activity';
 export * from './project/i-project-roadmap-entry';
---
+++ packages/cloutscript-extras/src/interfaces/passthrough/canonical-passthrough-routes.enum.ts
@@ -0,0 +1,9 @@
+export enum PassthroughRoutes {
+    CLOUT = "/clout",
+    DATAREQUEST = "/data",
+    STORAGEREQUEST = "/object",
+    LOGREQUEST = "/log",
+    QUEUEREQUEST = "/queue",
+    IOTPERMS = "/iotperms",
+    STEPINVOKE = "/stepinvoke"
+}
