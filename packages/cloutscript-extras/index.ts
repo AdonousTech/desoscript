d5a753ade82d2b40e02e5956551406bb57edbc24
Matt Pitts <mattapitts@gmail.com>
2021-08-26 13:40:44 -0700

    fix(extras): simplify exports

--- packages/cloutscript-extras/index.ts
+++ packages/cloutscript-extras/index.ts
@@ -3,4 +3,16 @@
  * 
  * @module cloutscript-extras
 */
-export * from './src';
+// clout
+
+// passthrough
+export * from './src/passthrough/canonical-cli-instructions.enum';
+export * from './src/passthrough/canonical-dli-instructions.enum';
+export * from './src/passthrough/canonical-oli-instructions.enum';
+export * from './src/passthrough/canonical-passthrough-errors.enum';
+export * from './src/passthrough/canonical-passthrough-routes.enum';
+// project
+export * from './src/interfaces/project/i-project-activity';
+export * from './src/interfaces/project/i-project-roadmap-entry';
+export * from './src/interfaces/project/i-project-roadmap';
+export * from './src/interfaces/project/i-project';
