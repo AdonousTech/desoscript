6c91227a6b43b02010f25c7dda01c75564f818f3
Matt Pitts <mattapitts@gmail.com>
2021-10-20 12:25:38 -0700

    fix(desoscript-core): Update IBalanceEntryResponse to include ProfileEntryResponse prop

--- packages/desoscript-core/src/interfaces/financial/i-balance-entry-response.ts
+++ packages/desoscript-core/src/interfaces/financial/i-balance-entry-response.ts
@@ -1,3 +1,4 @@
+import { IProfileEntryResponse } from "../users/i-profile-entry-response";
 
 /**
  * See
@@ -6,9 +7,18 @@
  * @interface IBalanceEntryResponse
  */
 export interface IBalanceEntryResponse {
-    HODLerPublicKeyBase58Check: string;
-    CreatorPublicKeyBase58Check: string;
-    HasPurchased: boolean;
-    BalanceNanos: number;
-    NetBalanceInMempool: number;
+  // The public keys are provided for the frontend
+  HODLerPublicKeyBase58Check: string;
+  // The public keys are provided for the frontend
+  CreatorPublicKeyBase58Check: string;
+
+  // Has the hodler purchased these creator coins
+  HasPurchased: boolean;
+  // How much this HODLer owns of a particular creator coin.
+  BalanceNanos: number;
+  // The net effect of transactions in the mempool on a given BalanceEntry's BalanceNanos.
+  // This is used by the frontend to convey info about mining.
+  NetBalanceInMempool: number;
+
+  ProfileEntryResponse: IProfileEntryResponse;
 }
