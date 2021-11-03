00e74a10680827da830a22aa0f22b538fb1ffc08
Matt Pitts <mattapitts@gmail.com>
2021-11-03 15:29:02 -0700

    feat (ds-ambient): Add new deso-ambient package

---
+++ packages/desoscript-ambient/README.md
@@ -0,0 +1,11 @@
+# `@adonoustech/desoscript-ambient`
+
+> TODO: description
+
+## Usage
+
+```
+const desoscriptAmbient = require('@adonoustech/desoscript-ambient');
+
+// TODO: DEMONSTRATE API
+```
---
+++ packages/desoscript-ambient/src/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Desoscript Ambient
+ * 
+ * @module desoscript-ambient
+*/
+
+// ***Interfaces****
+//notifications
+export * from './interfaces/notifications/i-ambient-notification-summary';
