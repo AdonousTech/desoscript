79176a7fe976321412fc9f84681117e671b4a4fa
Matt Pitts <mattapitts@gmail.com>
2021-11-03 16:03:24 -0700

    feat (ds-ambient): Start building out IAmbientNotificationSummary

--- packages/desoscript-ambient/src/interfaces/notifications/i-ambient-notification-summary.ts
+++ packages/desoscript-ambient/src/interfaces/notifications/i-ambient-notification-summary.ts
@@ -1,3 +1,36 @@
 export interface IAmbientNotificationSummary {
-    
+    notifications: {
+        likes: {
+            count: number;
+            phrase: 'likes'
+        },
+        reposts: {
+            count: number,
+            phrase: 'content reposts'
+        },
+        follows: {
+            count: number,
+            phrase: 'follows'
+        },
+        creatorcoin: {
+            count: number,
+            phrase: 'creator coin purchases'
+        },
+        creatorcointransfer: {
+            count: number,
+            phrase: 'creator coin transfers'
+        },
+        basictransfer: {
+            count: number,
+            phrase: 'DESO transfers'
+        },
+        nftbid: {
+            count: number,
+            phrase: 'NFT bids'
+        },
+        nftbidaccepted: {
+            count: number,
+            phrase: 'NFT bid acceptances'
+        }
+    }
 }
