25c89c2deea344005b445e647a366ef0d3d4ebb3
Matt Pitts <mattapitts@gmail.com>
2021-11-18 14:09:10 -0800

    feat (ds-core): Update INotificationLikeTxIndexMetadata interface

--- packages/desoscript-core/src/interfaces/notifications/i-notification-like-tx-index-metadata.ts
+++ packages/desoscript-core/src/interfaces/notifications/i-notification-like-tx-index-metadata.ts
@@ -1,1 +1,21 @@
-export interface INotificationLikeTxIndexMetadata {}
+export interface INotificationLikeTxIndexMetadata {
+    BlockHashHex: string;
+    TxnIndexInBlock: number;
+    TxnType: "LIKE";
+    TransactorPublicKeyBase58Check: string;
+    AffectedPublicKeys: [{PublicKeyBase58Check: string; Metadata: string;}];
+    TxnOutputs: [
+        {PublicKey: string;  AmountNanos: number; }
+    ],
+    BasicTransferTxindexMetadata: {
+        TotalInputNanos: number;
+        TotalOutputNanos: number;
+        FeeNanos: number;
+        UtxoOpsDump: any;
+        UtxoOps: any; // reserved
+    },
+    LikeTxindexMetadata: {
+        IsUnlike: boolean;
+        PostHashHex: string;
+    }
+}
